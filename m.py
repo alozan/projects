fb6474a3586aa546dca0162d3eb06810baf7f5df
Angel Eduardo Lozano Niño <alozan@users.noreply.github.com>
2024-01-03 12:14:31 -0600

    Update m.py

--- m.py
+++ m.py
@@ -3,27 +3,27 @@ import numpy as np
 
 
 print ("Primer matriz")
-aa= float(input("Primer :"))
-ab= float(input("segundo:"))
-ac= float(input("tercer :"))
-ad= float(input("cuarto :"))
-ae= float(input("quinto :"))
-af= float(input("sexto  :"))
-ag= float(input("septimo:"))
-ah= float(input("octavo :"))
-ai= float(input("noveno :"))
+aa= int(input("Primer :"))
+ab= int(input("segundo:"))
+ac= int(input("tercer :"))
+ad= int(input("cuarto :"))
+ae= int(input("quinto :"))
+af= int(input("sexto  :"))
+ag= int(input("septimo:"))
+ah= int(input("octavo :"))
+ai= int(input("noveno :"))
 
 
 print("Segunda Matriz")
-ba= float(input("Primer :"))
-bb= float(input("segundo:"))
-bc= float(input("tercer :"))
-bd= float(input("cuarto :"))
-be= float(input("quinto :"))
-bf= float(input("sexto  :"))
-bg= float(input("septimo:"))
-bh= float(input("octavo :"))
-bi= float(input("noveno :"))
+ba= int(input("Primer :"))
+bb= int(input("segundo:"))
+bc= int(input("tercer :"))
+bd= int(input("cuarto :"))
+be= int(input("quinto :"))
+bf= int(input("sexto  :"))
+bg= int(input("septimo:"))
+bh= int(input("octavo :"))
+bi= int(input("noveno :"))
 
 ta=time.time()
 A = np.array([[aa,ab,ac],
@@ -33,27 +33,52 @@ B = np.array([[ba,bb,bc],
               [bd,be,bf],
               [bg,bh,bi]])
 
-C = A+B
+C = A + B
 tb=time.time()
 
 print(C)
 print(tb-ta)  
 
 tc=time.time()
-D = A*B
-E = D*C
-F = E*A
-G = A*A*A*A*A*A*A*A*A*A*A*A*A*A*A*A*A*A
-H = B*B*B*B*B*B*B*B*B*B*B*B*B*B*B*B*B*B
-I = G*H
-J = np.dot(A,B)
+D = np.multiply(A,B)
+E = np.multiply(D,C)
+F = np.multiply(E,B)
+G = np.multiply(A,A,A)
+H = np.multiply(B,B,B)
+I = np.multiply(G,H)
+J = np.dot(I,H)
+K = np.dot(G,H)
+L = np.dot(E,A)
+M = np.dot(G,H)
+N = np.multiply(A,A,A)
+O = np.multiply(B,B,B)
+P = np.multiply(G,N)
+Q = np.multiply(H,O)
 td=time.time()
 
-print(D)
-print(E)
-print(F)
-print(I)
-print(J)
-print(td-tc)
 
+print("C")
+print(C)
+print("D")
+print(D)
+print("E")
+print(E)
+print("F")
+print(F)
+print("I")
+print(I)
+print("J")
+print(J)
+print("K")
+print(K)
+print("L")
+print(L)
+print("M")
+print(M)
+print("P")
+print(P)
+print("Q")
+print(Q)
+print(td-tc)
+print(td-ta)
 
